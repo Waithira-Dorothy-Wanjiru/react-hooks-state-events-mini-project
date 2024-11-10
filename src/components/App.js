74adcdcafec2877ba96361ffb3d50306fb33d7cd
Dorothy <dorothywanjiru32@gmail.com>
2024-11-10 14:41:31 +0300

    2nd commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
+import TaskList from "./TaskList";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
+
 import { CATEGORIES, TASKS } from "../data";
 
 function App() {
   const [tasks, setTasks] = useState(TASKS);
   const [selectedCategory, setSelectedCategory] = useState("All");
 
-  const handleDeleteTask = (taskText) => {
-    setTasks(tasks.filter(task => task.text !== taskText));
+
+  const handleDeleteTask = (taskId) => {
   };
 
   const handleAddTask = (newTask) => {
